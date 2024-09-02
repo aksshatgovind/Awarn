d01405c5e286f3dae2afaf5bdcca7cbbf67e2fbc
aksshatgovind <105073216+aksshatgovind@users.noreply.github.com>
2024-09-02 15:36:10 +0530

    AWARN v2.0

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Avalanche from './screens/avalanche'
+import Home from './screens/home'
+import About from './screens/about'
+
+import Guide from './screens/guide'
+import Flood from './screens/flood'
+import Region2 from './screens/region2'
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/Avalanche" element={<Avalanche />} />
+          <Route exact path="/about" element={<About/>} />
+          <Route exact path="/guide" element={<Guide/>} />
+          <Route exact path="/Flood" element={<Flood/>} />
+          <Route exact path="/Region2" element={<Region2/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
-
 export default App;
